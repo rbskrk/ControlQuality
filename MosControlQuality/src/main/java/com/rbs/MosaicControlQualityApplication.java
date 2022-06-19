448ffcbb5af7139240adb4ff424014f7ae30bf54
rbs <rbs@DESKTOP-J3H9L2F>
2022-06-19 18:52:29 -0300

    ControlQuality

---
+++ MosControlQuality/src/main/java/com/rbs/MosaicControlQualityApplication.java
@@ -0,0 +1,13 @@
+package com.rbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MosaicControlQualityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MosaicControlQualityApplication.class, args);
+	}
+
+}
